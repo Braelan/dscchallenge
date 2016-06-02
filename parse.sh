7585adaddca9be0941d0bc907b57f7ca031349b3
Braelan <bbraelan@gmail.com>
2016-06-02 16:42:14 -0700

    incremental progress

--- parse.sh
+++ parse.sh
@@ -4,30 +4,34 @@ FIFTY=0
 FORTY=0
 THIRTY=0
 TWENTY=0
-
-for i in `grep -o "\s\b[0-9][0-9][0-9]\b\s" sample.log `; do
-  echo "$i" 
-   case "$i" in 
+ARRAY=()
+grep -o "[GPD][EU][TL].*\s\b[0-9][0-9][0-9]\b\s" sample.log | 
+while read -r line; do
+  echo $line
+  code=`echo "$line" | grep -o "[0-9][0-9][0-9]"`
+  echo "$code"  
+   case $code in 
     [5]*)
-      ((FIFTY++))
+       $FIFTY=$((FIFTY+1))
+      $ARRAY+="$line"
     ;;
      [4]*)
-      ((FORTY++))
+      $FORTY=$++))
     ;;
      [3]*)
-      ((THIRTY++))
+      (($THIRTY++))
     ;;
      [2]*)
-      ((TWENTY++))
+      (($TWENTY++))
     ;;
    esac
-done
+ done
 
 echo "50x: $FIFTY |s"
 echo "40x: $FORTY |s"
 echo "30x: $THIRTY |s"
 echo "20x: $TWENTY |s"
 
-
+#echo ${ARRAY[*]}
 
 
